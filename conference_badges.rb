38ffb7ec3e03f54fc81e667f651c7f20bfb372cb
ansara.hooks <ansara.hooks@gmail.com>
2019-02-27 21:17:56 +0000

    Automatically backed up by Learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -2,10 +2,10 @@ def badge_maker (name)
   "Hello, my name is #{name}."
 end
 
-def batch_badge_creator(names)
-  greetings = []
-  names.each do |name|
-    greetings <<  badge_maker(name)
-  end
-  return greetings 
-end
+#def batch_badge_creator(names)
+#  greetings = []
+#  names.each do |name|
+  #  greetings <<  badge_maker(name)
+  #end
+#  return greetings
+# end
